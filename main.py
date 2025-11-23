7b9d483bb9222a6e025de2f88278ec450d7d789e
Henrique Petiz <heishenrique@gmail.com>
2025-11-23 13:15:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,15 +18,12 @@ except Exception:
 
 
 
+#### importa as bibliotecas necessárias
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-
-
-
+import seaborn  as sns
 
 ### Printa as colunas para melhor organização do código
 
@@ -46,7 +43,22 @@ df_exploded_genres = df.explode('Genres')
 ### Converte a coluna de Pontuação Metacritic para numérico
 df['Metacritic score'] = pd.to_numeric(df['Metacritic score'], errors='coerce')
 
+# Remove linhas duplicadas, mantendo a primeira ocorrência do jogo ('Name')
+df_limpo_p1 = df.drop_duplicates(subset=['Name'], keep='first').copy()
+
 # Removendo linhas onde a pontuação Metacritic é NaN, pois é o foco da P1
-df.dropna(subset=['Metacritic score'], inplace=True)
+df_metacritic_validos = df.dropna(subset=['Metacritic score']).copy()
 
+###ordenar pontuação  de forma decrescente
+top_10_ordenado = df_metacritic_validos.sort_values(
+    by=['Metacritic score', 'release_date'], 
+    ascending=[False, True]
+).head(10)
 
+### Mostra as colunas desejadas
+colunas_para_mostrar = ['Name', 'Metacritic score', 'release_date', 'Developers']
+
+print("Top 10 Jogos no Steam (P1 - Ordenação de Dois Níveis):")
+
+with pd.option_context('display.max_columns', None, 'display.width', 1000):
+    print(top_10_ordenado[colunas_para_mostrar])
